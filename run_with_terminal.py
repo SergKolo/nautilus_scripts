c27126ca613d4e9fedfb6ee34aa8a226f4ac4d2f
SergKolo <1047481448@qq.com>
2016-09-09 05:17:18 -0600

    reformated with main function

--- run_with_terminal.py
+++ run_with_terminal.py
@@ -3,7 +3,11 @@ from os import path
 from sys import argv
 from subprocess import call
 
-for item in argv[1:]:
-    full_path = path.abspath('./' + item)
-    call(['gnome-terminal','-e', 
-          "bash -c '" +  full_path + ";read'"])
+def main():
+    for item in argv[1:]:
+        full_path = path.abspath('./' + item)
+        call(['gnome-terminal','-e', 
+              "bash -c '" +  full_path + ";read'"])
+
+if __name__ == '__main__':
+     main()
