006b01d3b927899efdcd1793d7cf412e7c3bcdea
SergKolo <1047481448@qq.com>
2016-09-08 22:48:33 -0600

    added script for running applications with terminal

---
+++ run_with_terminal.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+from os import path
+from sys import argv
+from subprocess import call
+
+for item in argv[1:]:
+    full_path = path.abspath('./' + item)
+    call(['gnome-terminal','-e', full_path])
